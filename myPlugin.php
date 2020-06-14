3188106db79893e3599ad2219eca151560e0cc72
Andrii Boiko <boiko.andrii@gmail.com>
2020-06-14 20:53:54 +0300

    Initializing activation/deactivation hooks.

--- myPlugin.php
+++ myPlugin.php
@@ -6,3 +6,34 @@
  * Requires PHP:      7.2
  * Author:            andriiboiko
  */
+
+/**
+ * Register the "Instruction" custom post type
+ */
+function pluginprefix_setup_post_type() {
+    register_post_type( 'Instruction', ['public' => true] );
+}
+add_action( 'init', 'pluginprefix_setup_post_type' );
+
+/**
+ * Activate the plugin
+ */
+function pluginpostfix_activate() {
+    // Trigger function that registers the custom post type plugin.
+    pluginprefix_setup_post_type();
+    // Clear the permalinks after the post type has been registered.
+    flush_rewrite_rules();
+}
+
+register_activation_hook( __FILE__, 'pluginprefix_activate' );
+
+/**
+ * Deactivate the plugin
+ */
+function pluginprefix_deactivate() {
+    unregister_post_type( 'Instruction' );
+    // Clear the permalinks to remove post type's rules from the database
+    flush_rewrite_rules();
+}
+
+register_deactivation_hook( __FILE__, 'pluginpostfix_deactivate' );
